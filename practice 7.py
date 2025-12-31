737602a559b158c969d664558ce99fb3b7e4bfd9
akash5020J <aandiboy@student.gitam.edu>
2025-12-31 10:56:46 +0530

    marks grade

---
+++ practice 7.py
@@ -0,0 +1,20 @@
+marks = float(input("Enter your marks (0-100): "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 80:
+    grade = "B"
+elif marks >= 70:
+    grade = "C"
+elif marks >= 60:
+    grade = "D"
+elif marks >= 50:
+    grade = "E"
+else:
+    grade = "F"
+
+print(f"Your grade is {grade}")
+
+OUTPUT:
+Enter your marks (0-100): 86
+Your grade is B
